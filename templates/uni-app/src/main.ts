55b4ca442c2330f006c1381d9fba643df313ecbf
zhengxs <zhengxs2018@foxmail.com>
2022-03-19 15:03:43 +0800

    Update main.ts

--- templates/uni-app/src/main.ts
+++ templates/uni-app/src/main.ts
@@ -1,8 +1,13 @@
 import { createSSRApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 
 export function createApp() {
   const app = createSSRApp(App)
+  const pinia = createPinia()
+
+  app.use(pinia)
 
   return {
     app
